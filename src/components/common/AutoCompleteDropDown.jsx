397011b899b1a01596cdba227917b8326e84cc40
Matthew Mayaki <dynamicmax5000@gmail.com>
2019-10-02 10:38:08 +0100

    Updated Task 3

--- src/components/common/AutoCompleteDropDown.jsx
+++ src/components/common/AutoCompleteDropDown.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import LOADER from '../../assets/loader.svg';
+//import ProviderList from '../ProviderList';
 
 class AutoCompleteDropDown extends React.Component {
   constructor(props) {
@@ -26,6 +27,9 @@ class AutoCompleteDropDown extends React.Component {
     // the value(s) entered as a query param. i.e
     // GET <BASE_URL>/api/providers?q={input}
     // Use the response to populate the dropdown.
+    // The dropdown should only show while the user is typing,
+    // and should also cater for situations where no result is available.
+    // NOTE: This component should be as reusable as possible.
   }
 
   onResultSelected = (selectedResult) => {
@@ -36,44 +40,18 @@ class AutoCompleteDropDown extends React.Component {
   }
 
   render() {
-    const { showResults, results, isLoading } = this.state;
-    const { customListComponent, placeholder } = this.props;
+    const { isLoading } = this.state;
+    const { placeholder } = this.props;
     return (
       <div className="dropdown active autocomplete-dropdown">
         <input
           type="text"
           className="input__style_1 input__search"
           placeholder={placeholder || "&#xf002; Find a Provider"}
-          onInput={this.getResults}
-          onChange={this.getResults}
-          onFocus={() => this.toggleResultsDropdown(true)}
-          onBlur={() => this.toggleResultsDropdown(false)}
         />
         {isLoading && <img src={LOADER} className="loader" alt="loading" />}
-        {showResults && (
-          customListComponent ? (
-            <div className="dropdown-menu">
-              {React.createElement(
-                customListComponent,
-                {
-                  providers: results,
-                  onResultSelected: this.onResultSelected
-                }
-              )}
-            </div>
-          ) : (
-            <ul className="dropdown-menu">
-              {results.map(result => (
-                <li
-                  key={result.id}
-                  onMouseDown={() => this.onResultSelected(result)}
-                >
-                  {result.name}
-                </li>
-              ))}
-            </ul>
-          ))
-        }
+
+        {/* <ProviderList providers={results} onResultSelected={this.onResultSelected} /> */}
       </div>
     );
   }
